969f3619456c8dc62f3f98bc8fd168782c1ca461
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-08-17 23:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function calculateMinCost() {
   //your code here
+	// Get rope lengths input value
+ var ropeLengthsInput = document.getElementById("rope-lengths").value;
+
+            // Convert the input string to an array of lengths (assuming comma-separated)
+  var ropeLengthsArray = ropeLengthsInput.split(",");
+  let sum=0;
+  while(ropeLengthsArray.length!=0){
+	  ropeLengthsArray.sort();
+	  let a=ropeLengthsArray.shift();
+	  let b=ropeLengthsArray.shift();
+	  sum+=(a+b)
+	  ropeLengthsArray.push(a+b);
+  }
   
-  
-  
+  return sum;
 }  
